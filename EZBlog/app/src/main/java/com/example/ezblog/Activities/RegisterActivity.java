f6ec40081bbc863a5e4e71c20c83cf66e00438a0
TheTaTha5 <ta-zaza2012@hotmail.com>
2022-03-30 23:11:34 +0700

    register page updated
    Design update(register page)

--- EZBlog/app/src/main/java/com/example/ezblog/Activities/RegisterActivity.java
+++ EZBlog/app/src/main/java/com/example/ezblog/Activities/RegisterActivity.java
@@ -1,14 +1,16 @@
-package com.example.ezblog;
+package com.example.ezblog.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.ezblog.R;
+
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
